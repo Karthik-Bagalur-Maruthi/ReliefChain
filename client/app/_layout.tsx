b9868893c0eef9afd9bf46aeb4df5c31ba668905
Karthik Bagalur Maruthi <karthikbm970@gmail.com>
2026-05-21 21:40:10 +0530

    Updated login routing

--- client/app/_layout.tsx
+++ client/app/_layout.tsx
@@ -1,9 +1,15 @@
 import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
 
 export default function RootLayout() {
   return (
-    <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="login" />
-    </Stack>
+    <>
+      <Stack initialRouteName="login" screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="login" />
+        <Stack.Screen name="(tabs)" />
+      </Stack>
+
+      <StatusBar style="auto" />
+    </>
   );
 }
