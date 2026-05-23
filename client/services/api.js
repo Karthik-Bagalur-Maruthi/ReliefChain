42f620924a29c5352ec80ad912eb78f7817b0656
Karthik Bagalur Maruthi <karthikbm970@gmail.com>
2026-05-23 08:04:30 +0530

    Production backend integration

--- client/services/api.js
+++ client/services/api.js
@@ -1,8 +1,5 @@
 import { Platform } from "react-native";
 
-const API_URL =
-  Platform.OS === "web"
-    ? "http://localhost:5000/api"
-    : "http://10.200.116.207:5000/api";
+const API_URL = "https://reliefchain-backend-4diq.onrender.com/api";
 
 export default API_URL;
